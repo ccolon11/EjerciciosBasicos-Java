0161d37093787c976dcfa5ac96a65054104b9143
Carlos Colón <121244569+ccolon11@users.noreply.github.com>
2023-01-14 11:23:25 -0400

    Add files via upload

---
+++ ejercicio3/StringArray.java
@@ -0,0 +1,14 @@
+package com.ejercicios.ejercicio3;
+
+public class StringArray {
+    public static void main(String[] args) {
+        String a [] = {"Cama","Mueble","Zapatos","Silla","Mesa",
+                "Comedor"};
+
+
+
+        for (int i =0; i < a.length; i++){
+               System.out.println(a[i]+" "+i);
+           }
+    }
+}
